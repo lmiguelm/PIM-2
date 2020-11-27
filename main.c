5886d67d5190e8c6c039df7ba39058a5142a1d91
lmiguelm <luismiguelfernandes.marcelo@gmail.com>
2020-11-27 16:55:43 -0300

    correção de ortografia

--- main.c
+++ main.c
@@ -1,19 +1,21 @@
-#include<stdio.h>
-#include<string.h>
-#include<time.h>
+#include <stdio.h>
+#include <string.h>
+#include <time.h>
 #include <stdlib.h>
 #include "./screens/login.c"
 #include "./models/unidade.c"
 
-void main() {
+void main()
+{
   int option;
 
-  do {
+  do
+  {
     system("cls");
     printf("\n\n================ Seja bem-vindo(a) ================\n");
     printf("\n[1] - Unidade I");
     printf("\n[2] - Unidade II");
-    printf("\n[3] - Unidade IIsssI");
+    printf("\n[3] - Unidade III");
     printf("\n[4] - Entrar como adminstrador");
     printf("\n\n[0] - Fechar");
     printf("\n\n==================================================\n\n");
@@ -21,23 +23,34 @@ void main() {
     printf("Digite: ");
     scanf("%d", &option);
 
-    if(option == 0) {
+    if (option == 0)
+    {
       exit(0);
-    } else if (option == 1 || option == 2 || option == 3 || option == 4) {
-     if(salvarUnidadeAtual(option) == 0) {
-       if(option == 4) {
-         login();
-       } else {
-        LoginScreen(option);
-       }
-     } else {
-       system("cls");
-       printf("Ops! nao foi possivel acessar essa unidade");
-     }
-    } else {
+    }
+    else if (option == 1 || option == 2 || option == 3 || option == 4)
+    {
+      if (salvarUnidadeAtual(option) == 0)
+      {
+        if (option == 4)
+        {
+          login();
+        }
+        else
+        {
+          LoginScreen(option);
+        }
+      }
+      else
+      {
+        system("cls");
+        printf("Ops! nao foi possivel acessar essa unidade");
+      }
+    }
+    else
+    {
       printf("Opcao invalida!!");
     }
     printf("\n\n");
     system("pause");
-  } while(true);
+  } while (true);
 }
